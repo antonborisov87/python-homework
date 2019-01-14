d58803cd468cd4cb92d4f59e65a067964ccdb4f2
antonborisov87 <44161405+antonborisov87@users.noreply.github.com>
2019-01-14 22:48:02 +0300

    zakommentiroval, ispravil oshibki

--- задание по инту/test1.py
+++ задание по инту/test1.py
@@ -1,5 +1,9 @@
-x = (477+854)**(1/3)*23+(2048-512)/16-29*12
+"""задание по инту:
+1. Create file with file name test.py
+2. Set the result of this expression to variable x and print it
+3. Print the result of dir() function with variable x as argument
+https://pp.userapi.com/c849032/v849032992/80fc3/nZKotjxIDoo.jpg"""
+
+x = int(round(477+854)**(1./3))*23+(2048-512)/16-29*12
 print(x)
 print(dir(x))
-
-print(8 ** (1 / 3))
--- задание по строкам/test2.py
+++ задание по строкам/test2.py
@@ -1,3 +1,14 @@
+"""Задание по строкам:
+1.1. Create file with file name test.py
+1.2. Create variable x and assign "London has a diverse range of people and cultures, and more than 300 languages are
+spoken in the region." string to it.
+1.3. Print this string with all letters uppercase.
+1.4. Print this string the index of first "c" letter in this string.
+1.5. Print the count of "o" letters in this string.
+1.6. Print a list of all words in this string. Should be - ['London', 'has', 'a', 'diverse', 'range', 'of', 'people',
+'and', 'cultures,', 'and', 'more', 'than', '300', 'languages', 'are', 'spoken', 'in', 'the', 'region.']
+1.7. Replace all "a" letters with "A" in this string."""
+
 x = "London has a diverse range of people and cultures, and more than 300 languages are spoken in the region."
 print(x.upper())
 print(x.find('c'))
@@ -5,4 +16,3 @@ print(x.count('o'))
 a = x.split()
 print(a)
 print(x.replace('a', 'A'))
-1331**(1/3)
