8c71b1f7afa945703e142ae65889ec2d729b776a
antonborisov87 <44161405+antonborisov87@users.noreply.github.com>
2019-01-14 17:12:37 +0300

    test

---
+++ задание по строкам/test2.py
@@ -0,0 +1,8 @@
+x = "London has a diverse range of people and cultures, and more than 300 languages are spoken in the region."
+print(x.upper())
+print(x.find('c'))
+print(x.count('o'))
+a = x.split()
+print(a)
+print(x.replace('a', 'A'))
+1331**(1/3)
