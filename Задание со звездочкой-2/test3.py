019d8e795123d2481fa04f4b0f201670b2d12241
antonborisov87 <44161405+antonborisov87@users.noreply.github.com>
2019-01-16 23:10:54 +0300

    dodelat nado

---
+++ Задание со звездочкой-2/test3.py
@@ -0,0 +1,30 @@
+L = """The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+A = 'antonborisov87@gmail.com'
+# в принте не забыть пробел когда соединяем:
+print(L + ' ' + A)
+print(len(L))
+vowels = 0
+for i in L:
+    if i in 'aeiouy':
+        vowels += 1
+print(vowels)
