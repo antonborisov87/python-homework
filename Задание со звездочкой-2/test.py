95fca6514300709a472be9d03c63c2472b9a15dc
antonborisov87 <44161405+antonborisov87@users.noreply.github.com>
2019-01-16 23:16:30 +0300

    dodelat posl zadanie

--- Задание со звездочкой-2/test.py
+++ Задание со звездочкой-2/test.py
@@ -1,3 +1,18 @@
+"""Задание со звездочкой-2:
+1. In console run command: python -c “import this”;
+2. Grab the output you get, and assign it to a string variable, let’s say “L” in a new python script (you can just copy
+-paste the text manually);
+3. Take your personal email as a second string and concatenate it with L;
+4. Print the length of L;
+5. Print the count all the vowels in L;
+6. Print each 18th symbol of the string, but do it in case opposite to the original (print A if the letter is a, print
+a if A etc.) adding the position of that letter in the string in format like:
+o 18,
+o 36E
+o 54
+o 72I
+o …"""
+
 L = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
